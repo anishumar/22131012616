d29d0bc83806f2090eae60e53c4afc774102bc43
anishumar <anishumar786@gmail.com>
2025-06-27 15:06:19 +0530

    Add initial implementation of URL shortener and logging middleware

---
+++ Logging Middleware/logger.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+class Config:
+    EMAIL: str = os.getenv("EMAIL")
+    NAME: str = os.getenv("NAME")
+    ROLLNO: str = os.getenv("ROLLNO")
+    ACCESSCODE: str = os.getenv("ACCESSCODE")
+    CLIENTID: str = os.getenv("CLIENTID")
+    CLIENTSECRET: str = os.getenv("CLIENTSECRET")
+
+    @classmethod
+    def get_config(cls) -> dict:
+        return {
+            "email": cls.EMAIL,
+            "name": cls.NAME,
+            "rollNo": cls.ROLLNO,
+            "accessCode": cls.ACCESSCODE,
+            "clientID": cls.CLIENTID,
+            "clientSecret": cls.CLIENTSECRET,
+        }
+
+
+class TokenManager:
+    API = "http://20.244.56.144/evaluation-service/auth"
+
+    expiry: int = 0
+    access_token: str = ""
+
+    @staticmethod
+    def get_bearer_access_token() -> str:
+        response = requests.post(
+            TokenManager.API,
+            json=Config.get_config(),
+        )
+        response.raise_for_status()
+        TokenManager.access_token = response.json().get("access_token", "")
+        TokenManager.expiry = response.json().get("expiry", 0)
+
+        return TokenManager.access_token
+
+    @staticmethod
+    def get_access_token() -> str:
+        if TokenManager.expiry <= 0:
+            return TokenManager.get_bearer_access_token()
+
+        if not TokenManager.access_token:
+            return TokenManager.get_bearer_access_token()
+
+        return TokenManager.access_token
+
+    @staticmethod
+    def get_headers() -> dict:
+        return {
+            "Authorization": f"Bearer {TokenManager.get_access_token()}",
+            "Content-Type": "application/json",
+        }
+    
+
+class Logger:
+    API = "http://20.244.56.144/evaluation-service/logs"
+    @staticmethod
+    def log(stack: str, level: str, package: str, message: str) -> None:
+        headers = TokenManager.get_headers()
+        payload = {
+            "stack": stack,
+            "level": level,
+            "package": package,
+            "message": message,
+        }
+        response = requests.post(Logger.API, json=payload, headers=headers)
+        response.raise_for_status()
+
+    @staticmethod
+    def debug(stack: str, package: str, message: str) -> None:
+        Logger.log(stack, "debug", package, message)
+
+    @staticmethod
+    def info(stack: str, package: str, message: str) -> None:
+        Logger.log(stack, "info", package, message)
+
+    @staticmethod
+    def error(stack: str, package: str, message: str) -> None:
+        Logger.log(stack, "error", package, message)
+    
+    @staticmethod
+    def warning(stack: str, package: str, message: str) -> None:
+        Logger.log(stack, "warn", package, message)
+
+    @staticmethod
+    def fatal(stack: str, package: str, message: str) -> None:
+        Logger.log(stack, "fatal", package, message)
